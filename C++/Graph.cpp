114fdaaa4ea729b7780f7a61eb9547da7ad45232
Bramix <Bramixtop@gmail.com>
2017-07-16 16:58:20 +0300

    BFS release

---
+++ C++/Graph.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+class Graph {
+private :
+	queue <int> turn;
+public: 
+    vector <int> BFS (vector <vector<int> > & matrix, int start) {
+    	vector <bool> used(matrix.size(), true); 
+    	vector <int> price(matrix.size());
+        start --;
+		turn.push(start); 
+		used[start] = false;
+		price[start] = 0;  
+			while(! turn.empty()){
+				int end = turn.front();
+				turn.pop();
+					for (int i = 0; i < matrix[end].size(); i ++){
+						int buf = matrix[end][i];
+							if(used[buf]){
+								turn.push(buf);
+								used [buf] = false;
+								price [buf] = price[end] + 1;
+							 } 
+					}
+			}
+        return price;
+		//return used;  
+    }
+}; 
