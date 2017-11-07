05fae219c1d65a548c60319e2c64ac94df6665f5
Bramix <Bramixtop@gmail.com>
2017-11-07 12:51:01 +0200

    Update BFS.cpp

--- C++/BFS.cpp
+++ C++/BFS.cpp
@@ -11,20 +11,20 @@ public:
     	vector <bool> used(matrix.size(), true);
     	vector <int> price(matrix.size());
         start --;
-		turn.push(start);
-		used[start] = false;
-		price[start] = 0;
-			while(! turn.empty()){
-				int end = turn.front();
-				turn.pop();
-					for (int i = 0; i < matrix[end].size(); i ++){
-						int buf = matrix[end][i];
-							if(used[buf]){
-								turn.push(buf);
-								used [buf] = false;
-								price [buf] = price[end] + 1;
-							 }
-					}
+	turn.push(start);
+	used[start] = false;
+	price[start] = 0;
+		while(! turn.empty()){
+			int end = turn.front();
+			turn.pop();
+			for (int i = 0; i < matrix[end].size(); i ++){
+				int buf = matrix[end][i];
+				if(used[buf]){
+					turn.push(buf);
+					used [buf] = false;
+					price [buf] = price[end] + 1;
+				 }
+			}
 			}
         return price;
 		//return used;
